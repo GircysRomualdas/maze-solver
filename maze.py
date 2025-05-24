9c74aa9dce18d0b62d2bbfe8f83faf9905141d35
romas <romas@gmail.com>
2025-05-25 01:43:06 +0300

    add maze solver

--- maze.py
+++ maze.py
@@ -95,3 +95,40 @@ class Maze:
         for col in range(self.__num_cols):
             for row in range(self.__num_rows):
                 self.__cells[col][row].visited = False
+
+    def _solve_r(self, i, j):
+        self.__animate()
+        self.__cells[i][j].visited = True
+
+        if self.__cells[self.__num_cols - 1][self.__num_rows - 1] == self.__cells[i][j]:
+            return True
+
+        if i > 0 and not self.__cells[i - 1][j].visited and not self.__cells[i][j].has_left_wall and not self.__cells[i - 1][j].has_right_wall:
+            self.__cells[i][j].draw_move(self.__cells[i - 1][j])
+            if self._solve_r(i - 1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i - 1][j], True)
+        if i < self.__num_cols - 1 and not self.__cells[i + 1][j].visited and not self.__cells[i][j].has_right_wall and not self.__cells[i + 1][j].has_left_wall:
+            self.__cells[i][j].draw_move(self.__cells[i + 1][j])
+            if self._solve_r(i + 1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i + 1][j], True)
+        if j > 0 and not self.__cells[i][j - 1].visited and not self.__cells[i][j].has_top_wall and not self.__cells[i][j - 1].has_bottom_wall:
+            self.__cells[i][j].draw_move(self.__cells[i][j - 1])
+            if self._solve_r(i, j - 1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j - 1], True)
+        if j < self.__num_rows - 1 and not self.__cells[i][j + 1].visited and not self.__cells[i][j].has_bottom_wall and not self.__cells[i][j + 1].has_top_wall:
+            self.__cells[i][j].draw_move(self.__cells[i][j + 1])
+            if self._solve_r(i, j + 1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j + 1], True)
+
+        return False
+
+    def solve(self):
+        return self._solve_r(0, 0)
